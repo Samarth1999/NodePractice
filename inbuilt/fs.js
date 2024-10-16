dbcc9031ad7d3b5f9595f3c8e9e5641d9854b0bb
Samarth1999 <samarthvkamble99@gmail.com>
2024-10-16 14:28:58 +0530

    OS and FS file practiced

--- inbuilt/fs.js
+++ inbuilt/fs.js
@@ -1,5 +1,34 @@
 let fs = require('fs');
-fs.writeFile('mycode.txt','This is sample data',(err)=>{
-    if(err) throw err;
-    console.log('Task Done.')
-});
+
+// fs.writeFile('mycode.txt','This is sample data',(err)=>{
+//     if(err) throw err;
+//     console.log('Task Done.')
+// });
+
+//  fs.appendFile('myText.txt', 'Code for Node FS \n', (err)=>{
+//     if(err) throw err;
+//     console.log('File Appended');
+
+//  })
+
+    // fs.readFile('mycode.txt','utf-8',(err, data) => {   //utf-8 is a standard for incodding the file data
+    //     if (err) throw err;
+    //     console.log(data);
+    // })
+
+    //let data  = fs.readFileSync('city.json',{encoding:'utf-8',flag:'r'})
+    // console.log(data)
+    
+    // let data1  = fs.readFileSync('myText.txt',{encoding:'utf-8',flag:'r'})
+    // console.log(data1)
+    
+    
+    // fs.unlink('mycode.txt',(err)=>{    //for delete file
+    //     if(err) throw err;
+    //     console.log('file deleted')
+    // })
+    
+    fs.rename('myText.xml','myText.txt',(err)=>{  //for rename file
+        if(err) throw err;
+        console.log('file renamed')
+    })
